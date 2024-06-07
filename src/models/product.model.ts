d310bfca4f18ed6d8443c90576fddb56fe16d677
Lucas Borges <lucasvnborges@gmail.com>
2024-06-07 18:31:24 -0300

    feat: create product schema

---
+++ src/models/product.model.ts
@@ -0,0 +1,29 @@
+import { Schema, model, models } from "mongoose";
+
+const ProductSchema = new Schema({
+  creator: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  name: {
+    type: String,
+    required: [true, "name is required."],
+  },
+  price: {
+    type: String,
+    required: [true, "price is required."],
+  },
+  quantity: {
+    type: Number,
+    default: 1,
+    required: [true, "quantity is required."],
+  },
+  purchased: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const Product = models.Product || model("Product", ProductSchema);
+
+export default Product;
