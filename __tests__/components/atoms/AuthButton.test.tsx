598addad495b69604311f3e786b93bad60a0fc3d
Lucas Borges <lucasvnborges@gmail.com>
2024-06-08 13:18:18 -0300

    prettier fix

--- __tests__/components/atoms/AuthButton.test.tsx
+++ __tests__/components/atoms/AuthButton.test.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
-import AuthButton from '@components/atoms/AuthButton';
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import AuthButton from "@components/atoms/AuthButton";
 
-describe('AuthButton', () => {
+describe("AuthButton", () => {
   const mockOnClick = jest.fn();
   const props = {
-    text: 'Login with Provider',
-    providername: 'ProviderName',
+    text: "Login with Provider",
+    providername: "ProviderName",
     onClick: mockOnClick,
   };
 
@@ -14,33 +14,33 @@ describe('AuthButton', () => {
     jest.clearAllMocks();
   });
 
-  test('renders with provided text and image', () => {
+  test("renders with provided text and image", () => {
     render(<AuthButton {...props} />);
-    
+
     // Verifica se o texto é renderizado
-    expect(screen.getByText('Login with Provider')).toBeInTheDocument();
+    expect(screen.getByText("Login with Provider")).toBeInTheDocument();
 
     // Verifica se a imagem é renderizada com o src correto
-    const img = screen.getByRole('img', { name: /icon/i });
-    expect(img).toHaveAttribute('src', '/authProviders/providername.svg');
+    const img = screen.getByRole("img", { name: /icon/i });
+    expect(img).toHaveAttribute("src", "/authProviders/providername.svg");
   });
 
-  test('calls onClick when clicked', () => {
+  test("calls onClick when clicked", () => {
     render(<AuthButton {...props} />);
-    
-    const button = screen.getByRole('button');
+
+    const button = screen.getByRole("button");
     fireEvent.click(button);
-    
+
     expect(mockOnClick).toHaveBeenCalledTimes(1);
   });
 
-  test('changes opacity on hover', () => {
+  test("changes opacity on hover", () => {
     render(<AuthButton {...props} />);
-    
-    const button = screen.getByRole('button');
+
+    const button = screen.getByRole("button");
     fireEvent.mouseOver(button);
 
     // Checa se o estilo de opacidade muda para 0.5
-    expect(button).toHaveStyle('opacity: 0.5');
+    expect(button).toHaveStyle("opacity: 0.5");
   });
 });
