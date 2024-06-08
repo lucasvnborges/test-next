df82ceec4dae1152ae58ce5a0de6ab5c2491e7d0
Lucas Borges <lucasvnborges@gmail.com>
2024-06-07 22:48:08 -0300

    feat: add middleware to protect api routes

---
+++ src/middleware.ts
@@ -0,0 +1,30 @@
+// the idea of code below is available at the link below
+// https://nextjs.org/docs/app/building-your-application/authentication
+
+import { NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+
+const PROTECTED_API_ROUTES = ["/api/product"];
+
+const isAuthenticated = async (request: NextRequest) => {
+  const token: any = await getToken({ req: request });
+  return !!token && Date.now() <= token.exp * 1000;
+};
+
+export async function middleware(request: NextRequest) {
+  const isProtectedApiRoute = PROTECTED_API_ROUTES.some((route: string) =>
+    request.nextUrl?.pathname?.startsWith(route)
+  );
+
+  if (isProtectedApiRoute) {
+    const isAuth = await isAuthenticated(request);
+    if (!isAuth) {
+      return Response.json(
+        { success: false, message: "Authentication failed" },
+        { status: 401 }
+      );
+    }
+  }
+
+  return NextResponse.next();
+}
